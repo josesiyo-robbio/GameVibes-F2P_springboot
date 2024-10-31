bd721faab7bc9cb035a70b29bf69fd59f877aa4f
josesiyo-robbio <jose.robles.bo@gmail.com>
2024-10-31 00:07:25 -0600

    mvp

--- src/main/java/com/josesiyo_robbio/GameVibes_F2P/controller/RecommendationController.java
+++ src/main/java/com/josesiyo_robbio/GameVibes_F2P/controller/RecommendationController.java
@@ -1,10 +1,18 @@
 package com.josesiyo_robbio.GameVibes_F2P.controller;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.josesiyo_robbio.GameVibes_F2P.dto.GameInfo;
 import com.josesiyo_robbio.GameVibes_F2P.dto.RecommendationDTO;
 import com.josesiyo_robbio.GameVibes_F2P.service.GameRecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/recommendations")
@@ -14,10 +22,10 @@ public class RecommendationController {
     private GameRecommendationService gameRecommendationService;
 
     @Autowired
-    private RestTemplate restTemplate; // Se inyecta RestTemplate
+    private RestTemplate restTemplate;
 
     @PostMapping
-    public String getGameRecommendations(@RequestBody RecommendationDTO recommendationDTO) {
+    public List<GameInfo> getGameRecommendations(@RequestBody RecommendationDTO recommendationDTO) {
         String mood = recommendationDTO.getMood();
         String hour = recommendationDTO.getHour();
 
@@ -28,8 +36,20 @@ public class RecommendationController {
         // Llama a la API de FreeToGame
         String response = restTemplate.getForObject(url, String.class);
 
+        // Parsea la respuesta JSON y extrae los campos necesarios
+        JsonArray gamesArray = JsonParser.parseString(response).getAsJsonArray();
+        List<GameInfo> gameInfoList = new ArrayList<>();
 
+        for (JsonElement gameElement : gamesArray) {
+            JsonObject game = gameElement.getAsJsonObject();
+            String title = game.get("title").getAsString();
+            String shortDescription = game.get("short_description").getAsString();
+            String genre = game.get("genre").getAsString();
 
-        return response; // Retorna la respuesta de la API
+            GameInfo gameInfo = new GameInfo(title, shortDescription, genre);
+            gameInfoList.add(gameInfo);
+        }
+
+        return gameInfoList;
     }
 }
---
+++ src/main/java/com/josesiyo_robbio/GameVibes_F2P/dto/GameInfo.java
@@ -0,0 +1,39 @@
+package com.josesiyo_robbio.GameVibes_F2P.dto;
+
+public class GameInfo {
+    private String title;
+    private String shortDescription;
+    private String genre;
+
+    // Constructor, getters y setters
+    public GameInfo(String title, String shortDescription, String genre) {
+        this.title = title;
+        this.shortDescription = shortDescription;
+        this.genre = genre;
+    }
+
+    // getters y setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
